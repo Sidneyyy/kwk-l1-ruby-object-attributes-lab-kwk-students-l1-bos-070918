de551ad80e9bf37407f9267e9f91ded2d0b656f8
Sidneyyy <catitude14@outlook.com>
2018-07-13 18:51:27 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,20 @@
 
 class Person
   
-  def 
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+end
